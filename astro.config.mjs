1130115610acf5d81b03300ce5a279686ac664df
Tobias Pruss <topruss@mail.de>
2025-10-22 00:11:58 +0200

    Fix: Vercel Adapter für Deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: vercel(),
   integrations: [tailwind()]
 });
